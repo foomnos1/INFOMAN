46bf13e86e043e9579271d8db628f8c3ef20d8f8
foomnos1 <foomnos.student@asiancollege.edu.ph>
2025-03-29 23:45:13 +0800

    files

---
+++ view.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>View Reservation</title>
+    <style>
+        /* Kamo na diri */
+    </style>
+</head>
+<body>
+    
+
+    <?php
+        include "connect.php";
+    ?>
+</body>
+</html>
