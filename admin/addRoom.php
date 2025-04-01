fbf0dddf7fefc9a4d187abe20ca7bac2f9046500
sheetup1 <129304662+sheetup1@users.noreply.github.com>
2025-04-01 20:21:08 +0800

    omnos

--- admin/addRoom.php
+++ admin/addRoom.php
@@ -10,7 +10,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Room Reservation</title>
+    <title>Create Room</title>
     <link rel="stylesheet" href="../baseColorAndTypeface.css">
     <style>
         /* Kamo na diri */
