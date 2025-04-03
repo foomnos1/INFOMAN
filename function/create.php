dffb5e46be18e39c3246e4d7ab1c4fa617c43c22
sheetup1 <129304662+sheetup1@users.noreply.github.com>
2025-04-03 19:21:25 +0800

    omnos

--- function/create.php
+++ function/create.php
@@ -12,11 +12,10 @@
 
         $newfilename = uniqid() . "-" . $filename;
 
-        move_uploaded_file($tmpName, '../uploads/' . $newfilename);
-
         $query = mysqli_query($con, "INSERT INTO `rooms`(`room_name`, `room_type`, `filename`, `price`, `room_availability`) VALUES ('$roomName','$roomType','$newfilename','$price','$availability')");
 
         if ($query){
+            move_uploaded_file($tmpName, '../uploads/' . $newfilename);
             header("Location: ../admin/admin.php");
         }
     }
