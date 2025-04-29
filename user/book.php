190abb02901937f1ddc87d009e8afb4a4318ecd8
foomnos1 <foomnos.student@asiancollege.edu.ph>
2025-04-29 09:41:18 +0800

    book done

--- user/book.php
+++ user/book.php
@@ -23,9 +23,14 @@
         header{
             display: flex;
             justify-content: space-between;
+            align-items: center;
             padding: 0 50px;
+            position: fixed;
+            left: 0;
+            right: 0;
+            top: 0;
             z-index: 1;
-            background: white;
+            background-color: white;
         }
 
         header .right{
@@ -41,25 +46,54 @@
             justify-content: center;
         }
 
-
-        .card{
-            background: white;
+        .form{
+            width: 75vw;
+            background-color: white;
             padding: 10px;
+            border-radius: 50px;
             display: flex;
             flex-direction: column;
             align-items: center;
-            width: 25vw;
-            height: 50vh;
-            justify-content: center;
-            border-radius: 25%;
+            gap: 10px;
         }
 
-        .card a{
-            background: var(--accent);
-            padding: 10px 20px;
-            color: white;
-            text-decoration: none;
+        .roomCard{
+            border: 1px solid black;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            padding: 5px;
+            width: 75%;
             border-radius: 25px;
+        }
+
+        .roomCard a{
+            padding: 10px 20px;
+            text-decoration: none;
+            background-color: var(--accent);
+            color: white;
+        }
+
+        .roomCard img{
+            width: 250px;
+        }
+
+        @media (max-width: 425px){
+            .roomCard img{
+                width: 150px;
+            }
+
+            header h1{
+                font-size: 16px;
+            }
+
+            header .right p{
+                display: none;
+            }
+
+            .form h1{
+                text-align: center;
+            }
         }
     </style>
 </head>
@@ -79,10 +113,25 @@
         </div>
     </header>
     <div class="container">
-        <form action="../function/book.php" method="post">
-            
+        <form action="" method="post">
+            <div class="form">
+                <h1>Select a Room</h1>
+                <?php
+                    $query = mysqli_query($con, "SELECT * FROM `rooms` WHERE `room_availability` = 'Available'");
+                    while ($row = mysqli_fetch_assoc($query)){
+                ?>
+                    <div class="roomCard">
+                        <img src="../images/roomPhotos/<?php echo $row['id']?>.jpg">
+                        <h3><?php echo $row['room_name']?></h3>
+                        <p>Room Type: <?php echo $row['room_type']?></p>
+                        <p>Price: <?php echo $row['price']?></p>
+                        <a href="time.php/?ID=<?php echo $row['id']?>">Use Room</a>
+                    </div>
+                <?php
+                    }
+                ?>
+            </div>
         </form>
     </div>
 </body>
 </html>
-
